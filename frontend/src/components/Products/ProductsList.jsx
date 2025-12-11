c48d4a89e5cac80bcead2ab9f26e3bc8161667f0
Nickscodes25 <nicolas.acero2023@gmail.com>
2025-12-11 15:29:58 -0500

    add list api

--- frontend/src/components/Products/ProductsList.jsx
+++ frontend/src/components/Products/ProductsList.jsx
@@ -1,14 +1,22 @@
+import { set } from "mongoose";
 import { useEffect, useState } from "react";
 
 const ProductsList = () => {
   const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    setProducts([
-      { _id: "1", name: "Producto A", price: 10, stock: 5 },
-      { _id: "2", name: "Producto B", price: 20, stock: 3 }
-    ]);
-  }, []);
+    fetch(`${process.env.API_URL}/products`)})
+    .then(response => response.json())
+    .then(data => setProducts(data))
+    .catch(error => {
+      console.error('error fetching products: ', error);
+      setError(error)
+    });
+  
+  if (error) {
+    return <p>Error fetching products: {error.message}</p>
+  }
 
   return (
     <div>
